681f29e8f17bc3fd85bb63dfacea6e8536307451
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2024-04-09 01:19:04 +0200

    chore(build): fix eslint error

--- public/inline-scripts/gtag.js
+++ public/inline-scripts/gtag.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 window.dataLayer = window.dataLayer || [];
 function gtag() {
-  // eslint-disable-next-line no-undef
   dataLayer.push(arguments);
 }
 gtag('js', new Date());
