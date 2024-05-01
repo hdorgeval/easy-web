b23f6b61b70382305ede3dbfb7f305fff156db11
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2024-05-02 01:27:17 +0200

    feat(dashboard): add header in graph

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -32,42 +32,45 @@ export const Dashboard: FC = () => {
           )}
         </div>
         <div
-          className="fs-6 mt-4 mx-4 text-start text-light"
+          className="card fs-6 mt-4 mx-0 text-start text-light"
           style={{ minWidth: '80vw', width: '80vw' }}
         >
-          <ResponsiveContainer width="100%" height={200}>
-            <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-              <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#f8f9fa" stopOpacity={1} />
-                  <stop offset="95%" stopColor="#f8f9fa" stopOpacity={0} />
-                </linearGradient>
-                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-              <XAxis dataKey="Date" interval="preserveStartEnd" />
-              <YAxis />
-              <CartesianGrid strokeDasharray="3 3" />
-              <Tooltip contentStyle={{ backgroundColor: 'rgb(28 32 35 / 96%)' }} />
-              <Legend verticalAlign="top" height={36} />
-              <Area
-                type="monotone"
-                dataKey="Clics"
-                stroke="#f8f9fa"
-                fillOpacity={1}
-                fill="url(#colorUv)"
-              />
-              <Area
-                type="monotone"
-                dataKey="Impressions"
-                stroke="#82ca9d"
-                fillOpacity={1}
-                fill="url(#colorPv)"
-              />
-            </AreaChart>
-          </ResponsiveContainer>
+          <div className="card-header">Résultat de recherches Google - impressions et clics</div>
+          <div className="card-body">
+            <ResponsiveContainer width="100%" height={200}>
+              <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                <defs>
+                  <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#f8f9fa" stopOpacity={1} />
+                    <stop offset="95%" stopColor="#f8f9fa" stopOpacity={0} />
+                  </linearGradient>
+                  <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                  </linearGradient>
+                </defs>
+                <XAxis dataKey="Date" interval="preserveStartEnd" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip contentStyle={{ backgroundColor: 'rgb(28 32 35 / 96%)' }} />
+                <Legend verticalAlign="top" height={36} />
+                <Area
+                  type="monotone"
+                  dataKey="Clics"
+                  stroke="#f8f9fa"
+                  fillOpacity={1}
+                  fill="url(#colorUv)"
+                />
+                <Area
+                  type="monotone"
+                  dataKey="Impressions"
+                  stroke="#82ca9d"
+                  fillOpacity={1}
+                  fill="url(#colorPv)"
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          </div>
         </div>
       </div>
     </PublicPageLayoutWithFixedBackgroundGradient>
